06a571ee561bc2d4ea8112f0905ae976bef9629a
Harry <harryyanarico19@gmail.com>
2025-04-11 20:48:07 -0400

    mensaje cambiado2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
  
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
-const host = process.env.PORT || 3000;
+const host = process.env.DB_HOST;
 
 app.get('/', (req, res) => {
     res.send('¡El servidor funciona!');
