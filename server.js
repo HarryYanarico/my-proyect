2b23356e31875db55c9d4db27c8514652e425b69
Harry <harryyanarico19@gmail.com>
2025-04-11 20:46:07 -0400

    mensaje cambiado

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
  
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
+const host = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('¡El servidor funciona!');
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor corriendo en ${host}:${PORT}`);
 });
